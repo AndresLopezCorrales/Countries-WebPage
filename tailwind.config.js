ba4da40ff14713fcc353e30086b4a6ebcfd067be
Andrés López Corrales <andreslopezcorrales326@gmail.com>
2024-12-04 18:44:51 -0700

    Mas colores y content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brown-super-hard': '#A45C40',
+        'brown-hard': '#C38370',
+        'brown-medium': '#E4B7A0',
+        'brown-low': '#F6EEE0',
+      },
+    },
   },
   plugins: [],
 }
